ecad4cb5fea7d7d38ef596f47dd6a9d30d146371
mph7 <85843263+mph7@users.noreply.github.com>
2022-10-08 13:09:43 -0300

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -o pipefail
+
+# Path to your Markdown files
+pandoc_path=${PANDOC_PATH:-./}
+# Path to your YML file
+yml_path=${YML_PATH:-./}
+# Path to put the exported ePub
+export_path=${EXPORT_PATH:-./export/}
+# Name of the exported file
+exported_file=${EXPORTED_FILE:-export.epub}
+# Branch to push the changes to
+branch=${PANDOC_BRANCH:-main}
+# Email id used while committing to the repo
+email=${EMAIL:-kawlost83783@gmail.com}
+# The commit message
+commit_message=${COMMIT_MESSAGE:-Updated Pandoc Exported Files}
+
+# build the ePub
+
+# cd ${pandoc_path}
+
+pandoc ${pandoc_path}*.md ${yml_path}*.yml -o ${export_path}${exported_file}
+
+# commit the new files
+git config --global user.email 
+git fetch
+git checkout ${branch}
+git add export/
+git commit -m "${commit_message}"
+git push origin ${branch}
